b54ad6a622bcb9ea4c9019ad7b089fc7b9aa3634
isaiahwong <iisaiah.dev@gmail.com>
2020-07-24 01:10:41 +0800

    Updated july solution

--- challenges/2020/4_2020_07_22_28_3399_Single_Number_3/main.go
+++ challenges/2020/4_2020_07_22_28_3399_Single_Number_3/main.go
@@ -1,9 +1,26 @@
 package main
 
-func singleNumber(nums []int) []int {
-	return nil
+import "fmt"
+
+// Naive implementation :(
+func singleNumber(nums []int) (res []int) {
+	unique := map[int]int{}
+	for _, n := range nums {
+		exists := unique[n]
+		if exists == 0 {
+			unique[n] = 1
+		} else {
+			unique[n] = 0
+		}
+	}
+	for k, v := range unique {
+		if v == 1 {
+			res = append(res, k)
+		}
+	}
+	return
 }
 
 func main() {
-
+	fmt.Println(singleNumber([]int{1, 2, 1, 3, 2, 5, 5, 2}))
 }
